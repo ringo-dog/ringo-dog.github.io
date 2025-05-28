b8f4026ca33509a7d6b6b3b81819f420f13e1924
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-05-29 01:23:06 +0300

    Update workout.js

--- works/fitness/js/workout.js
+++ works/fitness/js/workout.js
@@ -54,6 +54,7 @@ clickTrEnd=(id,name,spans,tds)=>db('img',(links)=>{/* ../X/img/drill/${+id+25}.p
   window.addEventListener('popstate',()=>{
     if(tds[0].textContent==='1')tds[2].textContent='00:00';for(let i=0;i<3;i++)spans[i].textContent=tds[i].textContent
     table.remove();allTime()
+    return false
   },{once:true})
 }),
 inBtns=(a)=>[10,15,30].reduce((str,t)=>str+inBtn('secondary',a+t),`<div class='btn-group btn-group-vertical'>`)+'</div>',
