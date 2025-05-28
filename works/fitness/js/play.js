0caefc39b355e8d4d782372ec71a28c7b7b9abd1
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-05-28 23:31:07 +0300

    Update play.js

--- works/fitness/js/play.js
+++ works/fitness/js/play.js
@@ -3,7 +3,7 @@ export default()=>db('img',(links)=>{
 /* img меньше высота или весь экран */
 T.insertAdjacentHTML('beforebegin',`<table id='table' class='fixed-top h-100 table table-responsive table-dark text-center overflow-hidden'><tr><th id='title' colspan='3'></th></tr><tr><td colspan='3' class='p-0'><img class='img-play'/></td></tr><tr><td id='timer' colspan='3' class='h2 p-3'></td></tr><tr><td colspan='3' class='p-0'><img class='img-play'/></td></tr><tr><td class='shadow-sm'><a>Назад</a></td><td class='shadow-sm'><a>Пауза</a></td><td class='shadow-sm'><a>Дальше</a></td></tr></table>`)
 let next,ar,interval,time,
-arr=Array.from(T.querySelectorAll('tr')).map(tr=>Array.from(tr.querySelectorAll('span')).map(el=>el.textContent).concat(tr.children[1].firstChild.textContent,tr.dataset.id)),
+arr=Array.from(T.querySelectorAll('tr')).map(tr=>Array.from(tr.querySelectorAll('span')).map(el=>el.textContent).concat(tr.children[1].firstChild.textContent,links[tr.dataset.id])),
 imgs=table.querySelectorAll('img'),
 pauses=['Подготовка','Отдых между упражнениями','Охлаждение'].map((el,i)=>[el,T.querySelectorAll(`caption.d-flex span`)[i].textContent]),
 pause=pauses.shift(),
@@ -13,7 +13,7 @@ end=()=>{
   let fun=()=>{synthVoice('Тренировка закончена');clear();table.remove()};
   pause=pauses.shift();if(pause[1]==='00:00'){fun()}else{rest[2](pause);next=fun}
 },
-funImg=()=>imgs[1].src=links[ar[4]],
+funImg=()=>imgs[1].src=ar[4],
 minus=()=>{time--;(time===0)?next():timer.textContent=inTime(time)},
 noneAdd=(el)=>el.classList.add('d-none'),
 noneRemove=(el)=>el.classList.remove('d-none'),
@@ -34,7 +34,8 @@ work=()=>{/* if(ar[1]) */
   if(ar[0]===0){ar=arr.shift();if(!ar){next=end;imgs[1].src='./img/plus.png'}else{next=route;funImg()}}else{next=rest[0];funImg()}
 };
 [()=>{clear();table.remove()},()=>alert('Пауза'),()=>next()].forEach((fun,i)=>table.querySelectorAll('a')[i].parentNode.onclick=fun)
-links=arr.map(ar=>links[ar[4]])
+// arr.forEach(ar=>ar[4]=links[ar[4]]);
+links=null
 utterance.lang='ru';utterance.volume=0.8;utterance.rate=0.8;utterance.pitch=0.8;
 // synthVoice=(text)=>alert(text);funImg=()=>imgs[1].src=`../X/img/drill/${+ar[4]+25}.png`
 ar=arr.shift();route();pause=pauses.shift();funImg()
