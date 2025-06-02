2494043f522630aca55039b7d69bfdc3b5627319
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-02 18:36:24 +0300

    Update function.js

--- works/fitness/js/function.js
+++ works/fitness/js/function.js
@@ -1,6 +1,9 @@
 let T=document.querySelector('table'),
-JS=(name,data)=>import(`./${name}.js`).then(m=>m.default(data)),/* только в workout */
+JS=(name,data)=>import(`./${name}.js`).then(m=>m.default(data)),
 db=(name,fun)=>import(`../db/${name}.js`).then(m=>fun(m.default)),
+inBtn=(c,t)=>`<button class='btn btn-sm btn-${c}'>${t}</button>`,
 inSec=(elem)=>{let ar=elem.textContent.split(':');return+ar[1]+ar[0]*60},
-inTime=(t)=>{t=new Date(0,0,0,0,0,t).toLocaleTimeString();if(t.slice(0,2)==='00')t=t.slice(3);return t}
-export{T,JS,db,inSec,inTime}
+inTime=(t)=>{t=new Date(0,0,0,0,0,t).toLocaleTimeString();if(t.slice(0,2)==='00')t=t.slice(3);return t},
+none={add:(el)=>el.classList.add('d-none'),remove:(el)=>el.classList.remove('d-none')},
+back=(fun)=>{history.pushState(null,null,'#');window.addEventListener('popstate',fun,{once:true})}
+export{T,JS,back,db,inBtn,inSec,inTime,none}
