1ff00c9911e9151959336cd1ab48be5f07d8e831
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-02 22:35:30 +0300

    Update group.js

--- works/fitness/js/group.js
+++ works/fitness/js/group.js
@@ -5,10 +5,7 @@ clicks=[,
   (tr)=>db('img',(links)=>{
     T.insertAdjacentHTML('beforebegin',`<table class='fixed-top h-100 table table-light text-center'><tr><th>${tr.textContent}</th></tr><tr><td class='px-0'><img src='${links[+tr.dataset.id]}' style='width:100vw'/></td></tr></table>`)
     //back(2,()=>T.previousElementSibling.remove())
-    back(2,()=>{
-      T.previousElementSibling.remove()
-      ;window.addEventListener('popstate',()=>table.remove(),{once:true})
-    })
+    history.pushState(null,null,'#2')
   }),
   (tr,arr,id)=>{none.add(tr);arr[id][1].push([+tr.dataset.id,1,'00:00','00:00']);localStorage.workoutMain=JSON.stringify(arr)}
 ],
@@ -30,9 +27,10 @@ page2=(name,index)=>db('drill',(arr)=>db('workout',(obj)=>{
     table.querySelector('.btn').classList.remove('active');let id=+select.value;clicks[0]=(tr)=>clicks[2](tr,arr,id)
   })
   table.querySelectorAll('tr[data-id]').forEach(tr=>tr.onclick=()=>clicks[0](tr))
-  funBack()
+  history.pushState(null,null,'#1')
   //back(1,()=>T.previousElementSibling.remove())
 }))
 db('workout-main',(arr)=>{
   select_str=arr.reduce((str,[[name]],i)=>`${str}<option value='${i}'>${name}</option>`,`<caption class='pt-0 input-group'><div class='w-50 input-group-prepend'>${inBtn('outline-secondary w-100 active','Посмотреть')}</div><select id='select' class='custom-select'><option selected disabled>Добавить в тренировку</option>`)+'</select></caption>';page1()
+  window.addEventListener('popstate',()=>T.previousElementSibling.remove())
 })
