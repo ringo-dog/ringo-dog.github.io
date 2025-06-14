efd94069dfa2d87d8172bff6626c7d4f522ef666
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-14 22:22:37 +0300

    Update function.js

--- works/fitness/js/function.js
+++ works/fitness/js/function.js
@@ -1,9 +1,14 @@
 let T=document.querySelector('table'),
 JS=(name,data)=>import(`./${name}.js`).then(m=>m.default(data)),
+beforebegin=(str)=>T.insertAdjacentHTML('beforebegin',str),
 db=(name,fun)=>import(`../db/${name}.js`).then(m=>fun(m.default)),
 inBtn=(c,t)=>`<button class='btn btn-sm btn-${c}'>${t}</button>`,
 inSec=(elem)=>{let ar=elem.textContent.split(':');return+ar[1]+ar[0]*60},
 inTime=(t)=>{t=new Date(0,0,0,0,0,t).toLocaleTimeString();if(t.slice(0,2)==='00')t=t.slice(3);return t},
 none={add:(el)=>el.classList.add('d-none'),remove:(el)=>el.classList.remove('d-none')},
-back=(p,fun)=>{history.pushState(null,null,'#'+p);window.addEventListener('popstate',fun,{once:true})}
-export{T,JS,back,db,inBtn,inSec,inTime,none}
+pushState=(page)=>history.pushState(null,null,`#${page}`),
+windowBack=()=>{
+  window.addEventListener('popstate',()=>T.previousElementSibling.remove())
+  /* ПКwindow.addEventListener('keypress',(e)=>{if(e.keyCode===127)history.back()}) */
+}
+export{T,JS,beforebegin,db,inBtn,inSec,inTime,none,pushState,windowBack}
