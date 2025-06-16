3077f57deb544723977baa9a8f685c8ea793f5a5
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-16 14:35:41 +0300

    Update workout-update.js

--- works/fitness/js/workout-update.js
+++ works/fitness/js/workout-update.js
@@ -88,10 +88,10 @@ touch=[
 ],
 numbers=()=>T.querySelectorAll('td:first-child').forEach((td,i)=>td.textContent=i+1),
 svgs=[
-`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="32" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 32 16">
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
 </svg>`,
-`<svg xmlns="http://www.w3.org/2000/svg" width="32" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 32">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
 </svg>`,
