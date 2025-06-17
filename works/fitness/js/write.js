f155cd55bcb263f8cbc2d69f552f4baf076209fc
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-17 15:17:14 +0300

    Update write.js

--- works/fitness/js/write.js
+++ works/fitness/js/write.js
@@ -1,1 +1,1 @@
-export default([key,obj,end])=>{/* alert(JSON.stringify(obj));end();return; */localStorage[key]=JSON.stringify(obj);if(end)end()}
+export default([key,obj,end])=>{localStorage[key]=JSON.stringify(obj);if(end)end()}
