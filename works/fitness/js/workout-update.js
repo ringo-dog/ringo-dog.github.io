153380d6be5433cd1e447b21b7be79de41a814ae
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-17 13:48:17 +0300

    Update workout-update.js

--- works/fitness/js/workout-update.js
+++ works/fitness/js/workout-update.js
@@ -3,27 +3,12 @@ export default([arr,funClick,funSave,funTrash])=>{
 
 let select_str,tr_click,x=[0,0],y=[0,0],
 inTr=(ar)=>ar.reduce((a,b)=>`${a}<td>${b}</td>`,'<tr>')+'</tr>',inTrArr=(str,ar)=>str+inTr(ar),
-// tableClass=['h4 text-center','table-sm table-responsive'].map(t=>()=>T.className=`fixed-top h-100 m-0 table table-light ${t}`),
-// inTrArr=(str,ar)=>str+ar.reduce((a,b)=>`${a}<td>${b}</td>`,'<tr>')+'</tr>',inTd=(a,b)=>`${a}<td>${b}</td>`,
-// page1=()=>{
-//   T.innerHTML=arr.reduce(inTrArr,'')
-//   T.querySelectorAll('tr').forEach((tr,i)=>tr.onclick=()=>{funClick(i,page2);tr_click=tr})
-//   T.className='fixed-top h-100 m-0 table table-light h4 text-center'
-// },
 page1=()=>{
   beforebegin(`<table id='table' class='fixed-top h-100 m-0 table table-light h4 text-center'></table>`)
   table.innerHTML=arr.reduce(inTrArr,'')
   table.querySelectorAll('tr').forEach((tr,i)=>tr.onclick=()=>{funClick(i,page2);tr_click=tr})
-  // T.querySelector('tr:last-child').onclick=page2
 },
 page2=(ar,index)=>db('drill',(drill)=>{
-  // let trueArr=(ar)=>{
-  //   start(ar[0]);ar=[drill,ar[1]];
-  //   append[0](ar[1].map(([id])=>[id,ar[0][id]]).reduce((str,ar)=>str+append[1](ar),''));ar=ar[1];
-  //   tbody.querySelectorAll('tr').forEach((tr,id)=>tr.querySelectorAll('span').forEach((span,i)=>span.textContent=ar[id][i+1]))
-  //   allTime()
-  // }
-  // /* заменить на выбор функции до page2 */if(ar)trueArr(ar)
   [
     ()=>{
       start(ar[0]);ar=[drill,ar[1]];
@@ -33,13 +18,10 @@ page2=(ar,index)=>db('drill',(drill)=>{
     },
     ()=>{
       start(['01:00','01:00','01:00',ar]);T.querySelectorAll('a').forEach(a=>a.classList['add']('invisible'))
-      // let playVisible=['add','remove'].map(key=>()=>T.querySelector('a:nth-child(2)').classList[key]('invisible'))
-      // start(['01:00','01:00','01:00',ar]);playVisible[0]()
       select.addEventListener('change',()=>{for(let i=0;i<2;i++)T.querySelector(`a:nth-child(${i+1})`).classList.remove('invisible')},{once:true})
     }
   ][index]()
   T.querySelectorAll('a').forEach((a,i)=>a.onclick=clickHead[i])
-  // ;[save,play,trash].forEach((fun,i)=>T.querySelector(`a:nth-child(${i+1})`).onclick=fun)
   T.querySelector('input').oninput=function(){if(this.value.length>30)this.value=this.value.slice(0,30)}
   T.querySelectorAll('.d-flex:nth-child(1n+2)').forEach(el=>el.onclick=clickCaption)
   if(!select_str)select.addEventListener('click',()=>db('workout-name',(names)=>db('workout',(obj)=>{
@@ -79,7 +61,7 @@ clickTr=(elem)=>{
 touch=[
   (e)=>{x[0]=e.changedTouches[0].screenX;y[0]=e.changedTouches[0].screenY},
   (e)=>{x[1]=e.changedTouches[0].screenX;y[1]=e.changedTouches[0].screenY;touch[2](e.target.closest('tr'))},
-  (tr)=>{/* ? несколько пикселей для направлений */
+  (tr)=>{
     x[2]=x[1]-x[0];y[2]=y[1]-y[0];if(x[2]+y[2]===0)return
     if(Math.abs(x[2])>Math.abs(y[2])){if(x[2]>0){tr.remove();numbers()}else{let clone=tr.cloneNode(true);tr.after(clone);touch[3](clone.firstChild,clone);tr.firstChild.innerText='*';clone.onclick=clickTr}allTime()}
     else{if(y[2]>0){tr.nextElementSibling.after(tr)}else{tr.previousElementSibling.before(tr)}numbers();tr.firstChild.innerText='*'}    
@@ -97,15 +79,10 @@ svgs=[
 </svg>`,
 `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
   <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-</svg>`, 
-`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-  <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z"/>
 </svg>`
 ],
 start=(arr)=>beforebegin(`<table id='T' class='fixed-top h-100 table table-responsive table-light'><caption class='h-2r d-flex justify-content-around text-center'>${svgs.reduce((str,el)=>`${str}<a class='w-25'>${el}</a>`,'')}</caption><caption class='pb-0 input-group'><input class='form-control text-center' value='${arr[3]}'/><div class='input-group-append'><span class='input-group-text'>03:00</span></div></caption>${[['secondary','подготовки'],['danger','отдыха между упражнениями'],['secondary','охлаждения']].reduce((str,ar,i)=>`${str}<caption class='d-flex justify-content-around alert-${ar[0]}'><small>Время ${ar[1]}</small><span>${arr[i]}</span></caption>`,'')}<tbody id='tbody'></tbody><caption class='pt-0 vw-100'><select id='select' class='custom-select'>${select_str}</select></caption></table>`),
 page3=(id,name,spans,tds)=>db('img',(links)=>{
-  /* links=`../X/img/drill/${+id+25}.png` */
   links=links[id]
   beforebegin(`<table id='T3' class='fixed-top h-100 table table-sm table-responsive table-light text-center'><tr><th colspan='4'>${name}</th></tr>${[['Повторений',inBtn('secondary','-'),spans[0].textContent,inBtn('secondary','+')],['Время',inBtns('-'),spans[1].textContent,inBtns('+')],['Отдых',inBtns('-'),spans[2].textContent,inBtns('+')]].reduce(inTrArr,'')}<tr><td colspan='4' class='p-0'><img src='${links}' style='width:100vw'/></td></tr></table>`)
   T3.querySelectorAll('button').forEach((btn,i)=>btn.onclick=()=>clicks[i](tds))
@@ -142,8 +119,6 @@ save=()=>{
   title=Array.from(T.querySelectorAll('caption span')).map(el=>el.textContent);
   title.unshift(T.querySelector('input').value);return[title,arr]
 }
-;
-page1()
+;page1()
 
-/* if(!localStorage.lastDrill)localStorage.lastDrill='[1,"00:30","00:00",9]'*/
-  }
+}
