5d1dc5013b165478de72263219038cc2debbf9df
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-17 15:22:39 +0300

    Update calendar.js

--- works/fitness/db/calendar.js
+++ works/fitness/db/calendar.js
@@ -1,2 +1,1 @@
 let obj;try{obj=JSON.parse(localStorage.calendar)}catch{obj={date:{},drill:[],name:[],rest:[]}}export default obj
-// export default(()=>{try{return JSON.parse(localStorage.calendar)}catch{return{date:[],drill:[],name:[],rest:[]}}})()
