6a59c536a9637cf3ee79ca4e994f6940f4e3ddfa
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-06-30 13:08:35 +0300

    Update play.js

--- works/fitness/js/play.js
+++ works/fitness/js/play.js
@@ -2,8 +2,9 @@ import{beforebegin,db,inSec,inTime,none}from'./function.js'
 export default(funEnd)=>db('img',(links)=>{
 
 beforebegin(`<table id='T3' class='fixed-top h-100 table table-responsive table-dark text-center overflow-hidden'><tr><th id='title' colspan='3'></th></tr><tr><td colspan='3' class='p-0'><img class='vw-100'/></td></tr><tr><td id='timer' colspan='3' class='h2 p-3'></td></tr><tr><td colspan='3' class='p-0'><img class='vw-100'/></td></tr><tr>${['<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>','<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z"/>'].map(el=>`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">${el}</svg>`).concat('Дальше').reduce((str,el)=>`${str}<td class='w-50 shadow-sm'>${el}</td>`,'')}</tr></table>`)
-let next,ar,interval,time,timeout,
+let next,ar,count,interval,time,timeout,
 arr=Array.from(T.querySelectorAll('tr')).map(tr=>Array.from(tr.querySelectorAll('span')).map(el=>el.textContent).concat(tr.children[1].firstChild.textContent,tr.dataset.id)),
+counts=['Десяты','Девяты','Восьмо','Седьмо','Шесто','Пяты','Четвёрты','Трети','Второ'],
 imgs=T3.querySelectorAll('img'),
 pauses=['Подготовка','Отдых между упражнениями','Охлаждение'].map((el,i)=>[el,T.querySelectorAll(`caption.d-flex span`)[i].textContent]),
 pause=pauses.shift(),
@@ -31,11 +32,12 @@ rest=[
 work=[
   ()=>{
     if(ar[1]==='00:00'){work[2]();next();return}
-    imgs[0].src=imgs[1].src;show([ar[4],ar[1],1,0]);if(ar[3])synthVoice(ar[3]);ar[0]--;if(ar[0]===0){work[2]()}else{next=rest[0];funImg()}
+    imgs[0].src=imgs[1].src;show([ar[4],ar[1],1,0]);if(ar[3])synthVoice(ar[3]);ar[0]--
+    if(ar[0]===0)return work[2]()
+    next=rest[0];funImg();count=counts.slice(-ar[0]).map(el=>`${el}й подход`)
+    if(count.length<ar[0]){let size=ar[0]-count.length,i=0;for(i;i<size;i++)count.unshift(null)}
   },
-  ()=>{
-    show([ar[4],ar[1],1,0]);synthVoice(`${ar[0]} подход`);ar[0]--;if(ar[0]===0){work[2]()}else{next=rest[0]}
-  },
+  ()=>{show([ar[4],ar[1],1,0]);ar[0]--;synthVoice(count[ar[0]]);if(ar[0]===0){work[2]()}else{next=rest[0]}},
   ()=>{ar=arr.shift();if(ar){next=route;funImg()}else{next=end;imgs[1].src=links}}
 ];
 [
