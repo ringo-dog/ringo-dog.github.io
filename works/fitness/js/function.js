0cc6672194a964e2d3b25772c258117a2c7fbd41
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-07-17 20:18:20 +0300

    Update function.js

--- works/fitness/js/function.js
+++ works/fitness/js/function.js
@@ -2,10 +2,11 @@ let T=document.querySelector('table'),
 JS=(name,data)=>import(`./${name}.js`).then(m=>m.default(data)),
 beforebegin=(str)=>T.insertAdjacentHTML('beforebegin',str),
 db=(name,fun)=>import(`../db/${name}.js`).then(m=>fun(m.default)),
+funBack=(fun)=>window.addEventListener('popstate',fun,{once:true}),
 inBtn=(c,t)=>`<button class='btn btn-sm btn-${c}'>${t}</button>`,
 inSec=(elem)=>{let ar=elem.textContent.split(':');return+ar[1]+ar[0]*60},
 inTime=(t)=>{t=new Date(0,0,0,0,0,t).toLocaleTimeString();if(t.slice(0,2)==='00')t=t.slice(3);return t},
 none={add:(el)=>el.classList.add('d-none'),remove:(el)=>el.classList.remove('d-none')},
 pushState=(page)=>history.pushState(null,null,`#${page}`),
 windowBack=()=>window.onpopstate=()=>T.previousElementSibling.remove()
-export{T,JS,beforebegin,db,inBtn,inSec,inTime,none,pushState,windowBack}
+export{T,JS,beforebegin,db,funBack,inBtn,inSec,inTime,none,pushState,windowBack}
