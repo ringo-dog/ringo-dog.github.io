bdbfc7e8e1dc62bf816fb1a8b93a745fd5fcd73d
ringo-dog <66195285+ringo-dog@users.noreply.github.com>
2025-07-17 20:21:09 +0300

    Update play.js

--- works/fitness/js/play.js
+++ works/fitness/js/play.js
@@ -1,4 +1,4 @@
-import{beforebegin,db,inSec,inTime,none}from'./function.js'
+import{beforebegin,db,funBack,inSec,inTime,none}from'./function.js'
 export default(funEnd)=>db('img',(links)=>{
 
 beforebegin(`<table id='T3' class='fixed-top h-100 table table-responsive table-dark text-center overflow-hidden'><tr><th id='title' colspan='3'></th></tr><tr><td colspan='3' class='p-0'><img class='vw-100'/></td></tr><tr><td id='timer' colspan='3' class='h2 p-3'></td></tr><tr><td colspan='3' class='p-0'><div class='w-100'></div><img class='vw-100'/></td></tr><tr>${['<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>','<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z"/>'].map(el=>`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">${el}</svg>`).concat('Дальше').reduce((str,el)=>`${str}<td class='w-50 shadow-sm'>${el}</td>`,'')}</tr></table>`)
@@ -49,8 +49,11 @@ work=[
 ].forEach((fun,i)=>T3.querySelector(`td.shadow-sm:nth-child(${i+1})`).onclick=fun)
 none.add(T3.querySelector('td.shadow-sm'))
 arr.forEach(ar=>ar[5]=links[ar[5]]);links=links[0]
-utterance.lang='ru';utterance.volume=1;utterance.rate=1.1;utterance.pitch=0.5;
-ar=arr.shift();funImg();route();pause=pauses.shift()
-window.addEventListener('popstate',clear,{once:true})
+utterance.lang='ru';utterance.volume=1;utterance.rate=1.1;utterance.pitch=0.5
+ar=arr.shift()
+funImg()
+route()
+pause=pauses.shift()
+funBack(clear)
 
 })
